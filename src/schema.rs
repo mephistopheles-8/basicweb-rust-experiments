79aacb87e56d84ccae26c28b64a5fc0a7f3dc131
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-13 19:55:58 -0400

    galleries table structure

---
+++ src/schema.rs
@@ -0,0 +1,42 @@
+table! {
+    galleries (id) {
+        id -> Integer,
+        kind -> Integer,
+        name -> Text,
+        description -> Text,
+        uuid -> Binary,
+        created -> Timestamp,
+        updated -> Timestamp,
+    }
+}
+
+table! {
+    gallery_items (id) {
+        id -> Integer,
+        gallery -> Integer,
+        resource -> Integer,
+        kind -> Integer,
+        name -> Text,
+        description -> Text,
+        uuid -> Binary,
+        created -> Timestamp,
+        updated -> Timestamp,
+    }
+}
+
+table! {
+    resources (id) {
+        id -> Integer,
+        filepath -> Text,
+        kind -> Integer,
+        uuid -> Binary,
+        created -> Timestamp,
+        updated -> Timestamp,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    galleries,
+    gallery_items,
+    resources,
+);
