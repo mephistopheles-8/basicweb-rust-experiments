68a5f82e34b8ff00fb7f058df148e29df720345f
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-14 17:47:03 -0400

    transoform into library; move server into examples directory

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,8 @@ extern crate diesel;
 #[macro_use]
 extern crate serde_json;
 
-use actix_files as fs;
 use handlebars::Handlebars;
-use actix_web::{middleware, web, App, HttpResponse, HttpServer};
-use actix_http::{body::Body, Response};
-use actix_web::dev::ServiceResponse;
-use actix_web::http::StatusCode;
-use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
+use actix_web::{web,HttpResponse};
 use actix_multipart::Multipart;
 use async_std::prelude::*;
 use futures::{StreamExt, TryStreamExt};
@@ -18,47 +13,13 @@ use diesel::r2d2::{self, ConnectionManager};
 use uuid::Uuid;
 use serde::{Serialize,Deserialize};
 
-mod schema;
-mod models;
+pub mod schema;
+pub mod models;
 
-type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
-async fn save_file(mut payload: Multipart) -> Result<HttpResponse, actix_web::Error> {
-    // iterate over multipart stream
-    while let Ok(Some(mut field)) = payload.try_next().await {
-        let content_type = field
-            .content_disposition()
-            .ok_or_else(|| actix_web::error::ParseError::Incomplete)?;
-        let filename = content_type
-            .get_filename()
-            .ok_or_else(|| actix_web::error::ParseError::Incomplete)?;
-        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
-        let mut f = async_std::fs::File::create(filepath).await?;
 
-        // Field in turn is stream of *Bytes* object
-        while let Some(chunk) = field.next().await {
-            let data = chunk.unwrap();
-            f.write_all(&data).await?;
-        }
-    }
-    Ok(HttpResponse::Ok().into())
-}
-
-fn file_upload() -> HttpResponse {
-    let html = r#"<html>
-        <head><title>Upload Test</title></head>
-        <body>
-            <form target="/" method="post" enctype="multipart/form-data">
-                <input type="file" multiple name="file"/>
-                <button type="submit">Submit</button>
-            </form>
-        </body>
-    </html>"#;
-
-    HttpResponse::Ok().body(html)
-}
-
-fn galleries_all ( conn: &SqliteConnection ) 
+pub fn galleries_all ( conn: &SqliteConnection ) 
     -> Result<Vec<models::Gallery>, diesel::result::Error> {
 
     use crate::schema::galleries::dsl::*;
@@ -66,7 +27,7 @@ fn galleries_all ( conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_by_id ( id0: i32, conn: &SqliteConnection ) 
+pub fn gallery_by_id ( id0: i32, conn: &SqliteConnection ) 
     -> Result<Option<models::Gallery>, diesel::result::Error> {
 
     use crate::schema::galleries::dsl::*;
@@ -77,7 +38,7 @@ fn gallery_by_id ( id0: i32, conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_by_uuid ( uuid0: Uuid, conn: &SqliteConnection ) 
+pub fn gallery_by_uuid ( uuid0: Uuid, conn: &SqliteConnection ) 
     -> Result<Option<models::Gallery>, diesel::result::Error> {
 
     use crate::schema::galleries::dsl::*;
@@ -88,7 +49,7 @@ fn gallery_by_uuid ( uuid0: Uuid, conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_items_by_gallery ( gallery_id: i32, conn: &SqliteConnection ) 
+pub fn gallery_items_by_gallery ( gallery_id: i32, conn: &SqliteConnection ) 
     -> Result<Vec<(models::GalleryItem, models::Resource)>, diesel::result::Error> {
 
     use crate::schema::gallery_items::dsl::*;
@@ -102,7 +63,7 @@ fn gallery_items_by_gallery ( gallery_id: i32, conn: &SqliteConnection )
 }
 
 
-fn gallery_item_by_id ( id0: i32, conn: &SqliteConnection ) 
+pub fn gallery_item_by_id ( id0: i32, conn: &SqliteConnection ) 
     -> Result<Option<(models::GalleryItem, models::Resource)>, diesel::result::Error> {
 
     use crate::schema::gallery_items::dsl::*;
@@ -116,7 +77,7 @@ fn gallery_item_by_id ( id0: i32, conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_item_by_id0 ( id0: i32, gallery0: i32, conn: &SqliteConnection ) 
+pub fn gallery_item_by_id0 ( id0: i32, gallery0: i32, conn: &SqliteConnection ) 
     -> Result<Option<(models::GalleryItem, models::Resource)>, diesel::result::Error> {
 
     use crate::schema::gallery_items::dsl::*;
@@ -130,7 +91,7 @@ fn gallery_item_by_id0 ( id0: i32, gallery0: i32, conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_item_by_uuid ( uuid0: Uuid, conn: &SqliteConnection ) 
+pub fn gallery_item_by_uuid ( uuid0: Uuid, conn: &SqliteConnection ) 
     -> Result<Option<(models::GalleryItem, models::Resource)>, diesel::result::Error> {
 
     use crate::schema::gallery_items::dsl::*;
@@ -144,7 +105,7 @@ fn gallery_item_by_uuid ( uuid0: Uuid, conn: &SqliteConnection )
     Ok(g0s)
 }
 
-fn gallery_create(
+pub fn gallery_create(
       name0 : &str
     , description0: &str
     , kind0: i32
@@ -165,7 +126,7 @@ fn gallery_create(
     Ok(uuid0)
 }
 
-fn gallery_item_create( 
+pub fn gallery_item_create( 
       name0 : &str
     , description0: &str
     , kind0: i32
@@ -214,7 +175,7 @@ fn resource_create (
     Ok(uuid0)
 }
 
-fn gallery_item_resource_create( 
+pub fn gallery_item_resource_create( 
       name0 : &str
     , description0: &str
     , kinds: (i32,i32)
@@ -239,7 +200,7 @@ fn gallery_item_resource_create(
 }
 
 
-async fn gallery_listing_json(
+pub async fn gallery_listing_json(
    pool: web::Data<DbPool>
   ) -> Result<HttpResponse,actix_web::Error> {
     let conn = pool.get().expect("couldn't get db connection from pool");
@@ -254,13 +215,13 @@ async fn gallery_listing_json(
 }
 
 #[derive(Serialize,Deserialize)]
-struct GalleryPost {
-    name : String,
-    description : String,
-    kind : i32,
+pub struct GalleryPost {
+    pub name : String,
+    pub description : String,
+    pub kind : i32,
 }
 
-async fn gallery_create_json(
+pub async fn gallery_create_json(
    data: web::Json<GalleryPost>, 
    pool: web::Data<DbPool>
  ) -> Result<HttpResponse,actix_web::Error> {
@@ -277,7 +238,7 @@ async fn gallery_create_json(
     Ok(HttpResponse::Ok().json(uuid))
  }
 
-async fn gallery_json(
+pub async fn gallery_json(
     path: web::Path<i32>
   , pool: web::Data<DbPool>
   ) -> Result<HttpResponse,actix_web::Error> {
@@ -296,7 +257,7 @@ async fn gallery_json(
     }
 }
 
-async fn gallery_items_json(
+pub async fn gallery_items_json(
     path: web::Path<i32>
   , pool: web::Data<DbPool>
   ) -> Result<HttpResponse,actix_web::Error> {
@@ -323,16 +284,16 @@ async fn gallery_items_json(
 }
 
 #[derive(Serialize,Deserialize)]
-struct GalleryItemPost {
-    name : String,
-    description : String,
-    kind : i32,
+pub struct GalleryItemPost {
+    pub name : String,
+    pub description : String,
+    pub kind : i32,
 }
 
 // first field is GalleryItemPost json
 // next field is the resource file
 
-async fn gallery_item_multipart(
+pub async fn gallery_item_multipart(
     mut payload: Multipart
   , path: web::Path<i32>
   , pool: web::Data<DbPool>
@@ -392,7 +353,7 @@ async fn gallery_item_multipart(
 
 
 
-async fn gallery_item_json(
+pub async fn gallery_item_json(
     path: web::Path<(i32,i32)>
   , pool: web::Data<DbPool>
   ) -> Result<HttpResponse,actix_web::Error> {
@@ -411,7 +372,7 @@ async fn gallery_item_json(
     }
 }
 
-async fn gallery_item_form(
+pub async fn gallery_item_form(
     path: web::Path<i32>,
     hb: web::Data<Handlebars<'_>>
     ) -> HttpResponse {
@@ -425,17 +386,7 @@ async fn gallery_item_form(
     HttpResponse::Ok().body(body)
 }
 
-async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
-    let data = json!({
-        "title": "Welcome"
-      , "parent" : "main"
-    });
-    let body = hb.render("content/index", &data).unwrap();
-
-    HttpResponse::Ok().body(body)
-}
-
-fn gallery_api_read( cfg: &mut web::ServiceConfig ) {
+pub fn gallery_api_read( cfg: &mut web::ServiceConfig ) {
     cfg
     .service(web::resource("/galleries")
         .route(web::get().to(gallery_listing_json))
@@ -454,7 +405,7 @@ fn gallery_api_read( cfg: &mut web::ServiceConfig ) {
     );
 }
 
-fn gallery_api_write( cfg: &mut web::ServiceConfig ) {
+pub fn gallery_api_write( cfg: &mut web::ServiceConfig ) {
     cfg
     .service(web::resource("/galleries")
         .route(web::post().to(gallery_create_json))
@@ -465,7 +416,7 @@ fn gallery_api_write( cfg: &mut web::ServiceConfig ) {
 }
 
 
-fn gallery_api( cfg: &mut web::ServiceConfig ) {
+pub fn gallery_api( cfg: &mut web::ServiceConfig ) {
     cfg
     .service(web::resource("/galleries")
         .route(web::get().to(gallery_listing_json))
@@ -487,96 +438,3 @@ fn gallery_api( cfg: &mut web::ServiceConfig ) {
 }
 
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-    dotenv::dotenv().ok();
-
-    // set up database connection pool
-    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
-
-    // Handlebar templates
-    
-    let mut handlebars = Handlebars::new();
-    handlebars
-        .register_templates_directory(".html", "./static/templates")
-        .unwrap();
-    let handlebars_ref = web::Data::new(handlebars);
-
-    HttpServer::new(move || {
-        App::new()
-            // db pool
-            .data(pool.clone())
-            // handlebars
-            .app_data(handlebars_ref.clone())
-            // error handlers
-            .wrap(error_handlers())
-            // logger (must be last)
-            .wrap(middleware::Logger::default())
-            // the gallery api
-            .configure(gallery_api)
-            .service(web::resource("/").route(web::get().to(index)))
-            .service(fs::Files::new("/", "./static/root/"))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
-
-// Error logic taken from examples
-
-// Custom error handlers, to return HTML responses when an error occurs.
-fn error_handlers() -> ErrorHandlers<Body> {
-    ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found)
-}
-
-// Error handler for a 404 Page not found error.
-fn not_found<B>(res: ServiceResponse<B>) -> actix_web::Result<ErrorHandlerResponse<B>> {
-    let response = get_error_response(&res, "Page not found");
-    Ok(ErrorHandlerResponse::Response(
-        res.into_response(response.into_body()),
-    ))
-}
-
-// Generic error handler.
-fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body> {
-    let request = res.request();
-
-    // Provide a fallback to a simple plain text response in case an error occurs during the
-    // rendering of the error page.
-    let fallback = |e: &str| {
-        Response::build(res.status())
-            .content_type("text/plain")
-            .body(e.to_string())
-    };
-
-    let hb = request
-        .app_data::<web::Data<Handlebars>>()
-        .map(|t| t.get_ref());
-   
-
-    match hb {
-        Some(hb) => {
-            let data = json!({
-                "title": error
-              , "parent" : "main"
-              , "error": error
-              , "status_code": res.status().as_str()
-            });
-            let body = hb.render("error", &data);
-
-            match body {
-                Ok(body) => Response::build(res.status())
-                    .content_type("text/html")
-                    .body(body),
-                Err(_) => fallback(error),
-            }
-        }
-        None => fallback(error),
-    }
-}
