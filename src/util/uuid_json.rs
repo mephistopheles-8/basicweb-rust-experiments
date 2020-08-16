1e0c83953d0537f2342e7e1056e62d37627043d8
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-16 00:50:45 -0400

    rename serde deserialization functions

--- src/util/uuid_json.rs
+++ src/util/uuid_json.rs
@@ -5,7 +5,7 @@
 use uuid::Uuid;
 use serde::{de,ser, Deserialize, Deserializer, Serializer};
 
-pub fn uuid_serialize<S>(v:&Vec<u8>,serializer:S) 
+pub fn serialize<S>(v:&Vec<u8>,serializer:S) 
     -> Result<S::Ok, S::Error>
     where S: Serializer
 {
@@ -20,7 +20,7 @@ pub fn uuid_serialize<S>(v:&Vec<u8>,serializer:S)
 }
 
 
-pub fn uuid_deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
     where D: Deserializer<'de>
 {
     let s = String::deserialize(deserializer)?;
