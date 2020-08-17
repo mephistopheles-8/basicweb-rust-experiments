f56ae7d47e2046bff1114a44067f3a299b79fbdc
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-16 20:54:44 -0400

    PostTree

--- src/models.rs
+++ src/models.rs
@@ -21,6 +21,42 @@ pub struct Post {
     pub updated: NaiveDateTime, 
 }
 
+#[derive(Serialize,Deserialize)]
+pub struct PostTree {
+    pub id: i32,
+    pub parent: Option<i32>,
+    pub depth: i32,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub body: String,
+    pub status: i32,
+    pub flagged: bool,
+    pub replies: Vec<PostTree>,
+    #[serde(with="uuid_json")]
+    pub uuid: Vec<u8>,
+    pub created: NaiveDateTime,
+    pub updated: NaiveDateTime, 
+}
+
+impl PostTree {
+   pub fn from_post( post: Post ) -> Self {
+        PostTree {
+          id : post.id
+        , parent : post.parent
+        , depth : post.depth
+        , title : post.title
+        , description : post.description
+        , body : post.body
+        , status: post.status
+        , flagged: post.flagged
+        , replies : vec![]
+        , uuid: post.uuid
+        , created: post.created
+        , updated: post.updated
+        }
+   }
+}
+
 #[derive(AsChangeset,Serialize,Deserialize)]
 #[table_name = "posts"]
 pub struct PostPost {
