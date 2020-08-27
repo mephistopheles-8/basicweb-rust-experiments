90019aa7d6ced605017acb922184bf8c5336d3ea
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-26 22:42:51 -0400

    board: Implement markdown editor client

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import Board from "./Board.html"
-
+import PostMarkdown from "./PostMarkdown.html"
 export default {
     board( target, props ){ 
         return  new Board({target,props}); 
+    },
+    postMarkdown( target, props ){ 
+        return  new PostMarkdown({target,props}); 
     }
 }
