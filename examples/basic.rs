da38b73a87f5c7c245f7a0055163d5cbaf436d58
M. Bellaire <22552422+mephistopheles-8@users.noreply.github.com>
2020-08-28 00:40:36 -0400

    update example with naming

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,5 +1,5 @@
 
-extern crate basic;
+extern crate catalog;
 #[macro_use]
 extern crate serde_json;
 
@@ -13,7 +13,7 @@ use diesel::r2d2::{self, ConnectionManager};
 use handlebars::Handlebars;
 use actix_web::{middleware, web, App, HttpServer};
 
-use basic::min_api;
+use catalog::min_api;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
